ec3285973c650f26c04798c826c3bdb4c37f7021
Deepak Challiyil <deepakchalliyil@Deepaks-Air.localdomain>
2022-10-16 15:21:29 -0400

    Refactor android app

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
 import android.view.SearchEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.opencsv.CSVWriter;
 
@@ -33,6 +42,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
@@ -56,68 +66,112 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static Boolean recording = false;
     private static LocalDateTime recordingStartTime;
     private static String selectedActivity = "IDLE";
+    private static int distanceTraversedInCm = 0;
+    private Long lastAccelSensorDisplayRefresh, lastGyroSensorDisplayRefresh;
+    private Long lastMagSensoryDisplayRefresh, lastLightIntensitySensorDisplayRefresh;
+
+    private TextView accelXTextView, accelYTextView, accelZTextView, gyroXTextView, gyroYTextView;
+    private TextView magXTextView, magYTextView, magZTextView;
+    private TextView lightIntensityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         registerButtons();
         registerSensors();
+        registerTextViews();
+
         startTime = System.currentTimeMillis();
         scheduleWriterThread();
 
     }
 
+    private boolean shouldDisplaySensorValuesInUI(long currentTimeInMillis, Long lastSensorDisplayRefresh) {
+        return lastSensorDisplayRefresh == null || currentTimeInMillis - lastSensorDisplayRefresh > 500;
+    }
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (!recording) {
+            return;
+        }
+        long currentTimeInMillis = System.currentTimeMillis();
+
         SensorData sensorData = dataMap.getOrDefault(event.timestamp, new SensorData(event.timestamp));
+
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             sensorData.setAcc_x(event.values[0]);
             sensorData.setAcc_y(event.values[1]);
             sensorData.setAcc_z(event.values[2]);
-//            dataMap.put(event.timestamp, sensorData);
+
+            if (shouldDisplaySensorValuesInUI(currentTimeInMillis, lastAccelSensorDisplayRefresh)) {
+                accelXTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[0]));
+                accelYTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[1]));
+                accelZTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[2]));
+                lastAccelSensorDisplayRefresh = currentTimeInMillis;
+            }
+
+
             try {
                 dataQueue.put(sensorData);
             } catch (InterruptedException e) {
                 System.out.println("Failed to add acceleration data to queue");
                 e.printStackTrace();
             }
-//            System.out.println("ACC:"+getTimeStamp().toString()+" :: "+ Arrays.toString(event.values));
+
         } else if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
             sensorData.setGyro_x(event.values[0]);
             sensorData.setGyro_y(event.values[1]);
-//            dataMap.put(event.timestamp, sensorData);
+
+            if (shouldDisplaySensorValuesInUI(currentTimeInMillis, lastGyroSensorDisplayRefresh)) {
+                gyroXTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[0]));
+                gyroYTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[1]));
+                lastGyroSensorDisplayRefresh = currentTimeInMillis;
+            }
+
             try {
                 dataQueue.put(sensorData);
             } catch (InterruptedException e) {
                 System.out.println("Failed to add gyro data to queue");
                 e.printStackTrace();
             }
-//            System.out.println("GYRO:"+getTimeStamp().toString()+" :: "+ Arrays.toString(event.values));
+
         } else if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             sensorData.setMag_x(event.values[0]);
             sensorData.setMag_y(event.values[1]);
             sensorData.setMag_z(event.values[2]);
-//            dataMap.put(event.timestamp, sensorData);
+
+            if (shouldDisplaySensorValuesInUI(currentTimeInMillis, lastMagSensoryDisplayRefresh)) {
+                magXTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[0]));
+                magYTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[1]));
+                magZTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[2]));
+                lastMagSensoryDisplayRefresh = currentTimeInMillis;
+            }
+
+
             try {
                 dataQueue.put(sensorData);
             } catch (InterruptedException e) {
                 System.out.println("Failed to add mag data to queue");
                 e.printStackTrace();
             }
-//            System.out.println("MAG:"+getTimeStamp().toString()+" :: "+ Arrays.toString(event.values));
+
         } else if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
             sensorData.setLux(event.values[0]);
-//            dataMap.put(event.timestamp, sensorData);
+            if (shouldDisplaySensorValuesInUI(currentTimeInMillis, lastLightIntensitySensorDisplayRefresh)) {
+                lightIntensityTextView.setText(String.format(Locale.ENGLISH, "%.2f", event.values[0]));
+                lastLightIntensitySensorDisplayRefresh = currentTimeInMillis;
+            }
+
             try {
                 dataQueue.put(sensorData);
             } catch (InterruptedException e) {
                 System.out.println("Failed to add light data to queue");
                 e.printStackTrace();
             }
-//            System.out.println("LIGHT:"+getTimeStamp().toString()+" :: "+event.values[2]);
+
         }
-//        flushData(event.timestamp);
     }
 
     @Override
@@ -125,65 +179,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    private void registerTextViews() {
+        accelXTextView = (TextView) findViewById(R.id.accelXValue);
+        accelYTextView = (TextView) findViewById(R.id.accelYValue);
+        accelZTextView = (TextView) findViewById(R.id.accelZValue);
+
+        gyroXTextView = (TextView) findViewById(R.id.gyroXValue);
+        gyroYTextView = (TextView) findViewById(R.id.gyroYValue);
+
+        magXTextView = (TextView) findViewById(R.id.magXValue);
+        magYTextView = (TextView) findViewById(R.id.magYValue);
+        magZTextView = (TextView) findViewById(R.id.magZValue);
+        lightIntensityTextView = (TextView) findViewById(R.id.LightIntensityValue);
+    }
+
     private void registerButtons() {
-        Button btnClick = (Button) findViewById(R.id.btnClick);
-        btnClick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView txtStatus = (TextView) findViewById(R.id.txtStatus);
-                txtStatus.setText("Hello");
-                v.setBackgroundColor(Color.parseColor("#ff00ff"));
+        SwitchCompat toggleSwitch = (SwitchCompat) findViewById(R.id.button1);
+        toggleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+                if (isChecked) {
+                    showCustomDialog();
+
+                }
+                else {
+                    recording = false;
+                }
             }
         });
 
-        Button btnAcc = (Button) findViewById(R.id.btnAcc);
-        btnAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView txtStatus = (TextView) findViewById(R.id.txtStatus);
-                txtStatus.setText("World!");
-                showAcc = true;
-
-            }
-        });
-
-        Button btnStart = (Button) findViewById(R.id.recordStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recording = true;
-                recordingStartTime = LocalDateTime.now();
-                btnStart.setEnabled(false);
-            }
-        });
-
-        Button btnEnd = (Button) findViewById(R.id.recordEnd);
-        btnEnd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recording = false;
-                btnStart.setEnabled(true);
-            }
-        });
-
-        Spinner dropdown = findViewById(R.id.spinner1);
-        String[] options = new String[]{"IDLE", "WALKING", "RUNNING", "STAIRS", "JUMPING"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, options);
-        dropdown.setAdapter(adapter);
-
-        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-                selectedActivity = item;
-                System.out.println("SELECTED: "+selectedActivity);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 
     private void registerSensors() {
@@ -205,10 +230,56 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private String getFileName() {
-        return selectedActivity + " "
-                + DateTimeFormatter.ofPattern("dd-MM-yy HH;mm;ss").format(recordingStartTime) + ".csv";
+        return selectedActivity + " " + DateTimeFormatter.ofPattern(
+                "dd-MM-yy HH;mm;ss").format(recordingStartTime
+        ) + " " + distanceTraversedInCm + "cm.csv";
     }
 
+    private void showCustomDialog() {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.activity_dialog);
+
+        Spinner dropdown = dialog.findViewById(R.id.spinner1);
+        String[] options = new String[]{"IDLE", "WALKING", "RUNNING", "STAIRS", "JUMPING"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item, options
+        );
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                selectedActivity = item;
+                System.out.println("SELECTED: "+selectedActivity);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        EditText distanceTraversedField = dialog.findViewById(R.id.distanceTraversedField);
+        Button submitButton = (Button) dialog.findViewById(R.id.actvitySubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String distanceTraversedFieldText = distanceTraversedField.getText().toString();
+                if (distanceTraversedFieldText.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter distance in cm", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                distanceTraversedInCm = Integer.parseInt(distanceTraversedFieldText);
+                recording = true;
+                recordingStartTime = LocalDateTime.now();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
     private void writeToFile(List<SensorData> data, boolean useHeader, String filename) {
         String[] header = {"Timestamp", "Accel X", "Accel Y", "Accel Z", "Gyro X", "Gyro Y", "Mag X", "Mag Y", "Mag Z", "Light Intensity"};
         List<String[]> csvData = new ArrayList<>();
@@ -241,7 +312,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             csvData.add(row);
         }
 
-        File file = new File(Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DOWNLOADS + "/" + filename);
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + filename);
         if(!file.exists()) {
             try {
                 file.createNewFile();
@@ -269,7 +340,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             @Override
             public void run() {
                 while(true) {
-//                    System.out.println("EXECUTING THREAD : "+dataQueue.size());
                     List<SensorData> writableData = new ArrayList<>();
                     dataQueue.drainTo(writableData, 2000);
 
@@ -283,7 +353,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                     if(idleCount[0] <= 2) {
                         try {
-//                            System.out.println("SLEEPING THREAD");
                             Thread.sleep(2000);
                         } catch (InterruptedException e) {
                             System.out.println("Writer thread failed to sleep");
